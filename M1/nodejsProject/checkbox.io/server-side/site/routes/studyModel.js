bec59f24f056b3a8f48ef829ea9a1408c76e52e7
svpageda <svpageda@ncsu.edu>
2017-02-16 19:04:23 -0500

    SMTP working code

--- M1/nodejsProject/checkbox.io/server-side/site/routes/studyModel.js
+++ M1/nodejsProject/checkbox.io/server-side/site/routes/studyModel.js
@@ -41,11 +41,11 @@ SurveyModel = function( incoming, token )
 	self.markdown = incoming.markdown;
 	self.token = token;
 
-	self.adminLink = "http://192.168.33.10:3002/studies/admin/?token=" + token;
+	self.adminLink = "http://192.168.33.10/studies/admin/?token=" + token;
 
 	self.setPublicLink = function(id)
 	{
-		self.publicLink = "http://192.168.33.10:3002/studies/?id=" + id;
+		self.publicLink = "http://192.168.33.10/studies/?id=" + id;
 	};
 
 
@@ -78,11 +78,11 @@ DataStudyModel = function( incoming, token )
 	self.markdown = incoming.markdown;
 	self.token = token;
 
-	self.adminLink = "http://192.168.33.10:3002/studies/admin/?token=" + token;
+	self.adminLink = "http://192.168.33.10/studies/admin/?token=" + token;
 
 	self.setPublicLink = function(id)
 	{
-		self.publicLink = "http://192.168.33.10:3002/studies/?id=" + id;
+		self.publicLink = "http://192.168.33.10/studies/?id=" + id;
 	};
 
 
